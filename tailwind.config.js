e8e825bf4470200b8bcd71dd177189e974d0c0ec
SongYi Kim <sykim@mz.co.kr>
2022-04-07 16:50:41 +0900

    feat: add Pretendard, work Sans fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires,import/no-extraneous-dependencies
+const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
     purge: [],
     theme: {
-        extend: {},
+        fontFamily: {
+            sans: ['Pretendard', ...defaultTheme.fontFamily.sans],
+            serif: ['Work Sans', ...defaultTheme.fontFamily.serif],
+        },
     },
     variants: {
         extend: {},
