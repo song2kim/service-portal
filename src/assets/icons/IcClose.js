1fc9a205e48242f81e7b6ce2694bf7b0c0745290
SongYi Kim <sykim@mz.co.kr>
2022-04-07 17:59:56 +0900

    feat: add icons and convert them to components

---
+++ src/assets/icons/IcClose.js
@@ -0,0 +1,16 @@
+import * as React from "react";
+
+const SvgIcClose = (props) => (
+  <svg
+    width={16}
+    height={16}
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path fill="#6638B6" d="m0 12.411 14.819-10.41 1.18 1.589L1.182 14z" />
+    <path fill="#6638B6" d="M1.182 2 16 12.41 14.82 14 0 3.59z" />
+  </svg>
+);
+
+export default SvgIcClose;
