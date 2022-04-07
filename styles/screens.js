a74f1c3aee1505f3eb598a6cfa5c42161f8e6295
SongYi Kim <sykim@mz.co.kr>
2022-04-07 16:52:20 +0900

    feat: add screen size for responsive

---
+++ styles/screens.js
@@ -0,0 +1,6 @@
+module.exports = {
+    desktop: { min: 1440 },
+    laptop: { min: 1024, max: 1439 },
+    tablet: { min: 768, max: 1023 },
+    mobile: { max: 767 },
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires,import/no-extraneous-dependencies
 const defaultTheme = require('tailwindcss/defaultTheme');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const screens = require('./styles/screens');
+
 module.exports = {
     purge: [],
     theme: {
         fontFamily: {
             sans: ['Pretendard', ...defaultTheme.fontFamily.sans],
             serif: ['Work Sans', ...defaultTheme.fontFamily.serif],
+        },
+        screens: {
+            desktop: { min: `${screens.desktop.min}px` },
+            laptop: { min: `${screens.laptop.min}px`, max: `${screens.laptop.max}px` },
+            tablet: { min: `${screens.tablet.min}px`, max: `${screens.tablet.max}px` },
+            mobile: { max: `${screens.mobile.max}px` },
         },
     },
     variants: {
