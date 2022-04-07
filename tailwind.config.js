7657949e37360d352b0e7dd84f537aafcdf2ff80
SongYi Kim <sykim@mz.co.kr>
2022-04-07 16:53:01 +0900

    fix: add color palettes

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,24 @@ module.exports = {
             tablet: { min: `${screens.tablet.min}px`, max: `${screens.tablet.max}px` },
             mobile: { max: `${screens.mobile.max}px` },
         },
+        colors: {
+            primary: '#6638B6',
+            violet: {
+                100: '#F8F8FC',
+                200: '#EBEAF6',
+                300: '#6638B6',
+                400: '#3C2C84',
+            },
+            gray: {
+                100: '#F6F6F6',
+                200: '#D8D8D8',
+                300: '#A3A3A3',
+                400: '#86868B',
+                500: '#484848',
+                600: '#1D1D1F',
+            },
+            white: '#FFFFFF',
+        },
     },
     variants: {
         extend: {},
