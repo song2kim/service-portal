e1523d1877f5382bc364d376ef80924b729947e9
SongYi Kim <sykim@mz.co.kr>
2022-04-13 17:11:23 +0900

    feat: add linkButton, linkText components

---
+++ src/components/linkButton.tsx
@@ -0,0 +1,40 @@
+import tw, { TwStyle } from 'twin.macro';
+
+type linkButtonColor = 'violet-400' | 'white'
+type linkButtonSize = 'medium' | 'large'
+
+interface LinkButtonProps {
+    color?: linkButtonColor;
+    size?: linkButtonSize;
+    children?: string;
+}
+
+const linkButtonColorStyle: Record<linkButtonColor, TwStyle> = {
+    white: tw`bg-white text-violet-500`,
+    'violet-400': tw`bg-violet-400 text-white hover:(bg-violet-500 text-white)`,
+};
+
+const linkButtonSizeStyle: Record<linkButtonSize, string> = {
+    medium: 'padding: 8px 16px',
+    large: 'padding: 12px 20px',
+};
+
+const styles = {
+    style: ({ color = 'violet-400', size = 'medium' }: LinkButtonProps) => [
+        tw`inline-block`,
+        linkButtonColorStyle[color],
+        linkButtonSizeStyle[size],
+    ],
+};
+
+const linkButton = ({ color, size, children }: LinkButtonProps) => (
+    <a href="/" css={styles.style({ color, size })}>{children}</a>
+);
+
+linkButton.defaultProps = {
+    color: 'violet-400',
+    size: 'medium',
+    children: 'Button',
+};
+
+export default linkButton;
---
+++ src/pages/example/linkButton.tsx
@@ -0,0 +1,41 @@
+import { FC } from 'react';
+import { useRouter } from 'next/router';
+import styled from 'styled-components';
+import tw from 'twin.macro';
+import LinkButton from '../../components/linkButton';
+
+const StyledContainer = styled.ul`
+  ${tw`bg-violet-200`}
+  display: flex;
+  flex-direction: column;
+  row-gap: 10px;
+  padding: 20px;
+`;
+
+const StyledItemRow = styled.li`
+    > a {
+      margin-right: 10px;
+    } 
+`;
+
+const Main: FC = () => {
+    const router = useRouter();
+    return (
+        <>
+            <StyledContainer>
+                <StyledItemRow><LinkButton>default button</LinkButton></StyledItemRow>
+                <StyledItemRow>
+                    <LinkButton color="violet-400" size="medium">violet / medium</LinkButton>
+                    <LinkButton color="violet-400" size="large">violet / large</LinkButton>
+                </StyledItemRow>
+                <StyledItemRow>
+                    <LinkButton color="white" size="medium">white / medium</LinkButton>
+                    <LinkButton color="white" size="large">white / large</LinkButton>
+                </StyledItemRow>
+            </StyledContainer>
+            <button style={{ width: '100%' }} type="button" onClick={() => router.back()}>go Back</button>
+        </>
+    );
+};
+
+export default Main;
