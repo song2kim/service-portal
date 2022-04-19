291f0e994b7d7ea36f5357ca3c360010f6dc5be3
SongYi Kim <sykim@mz.co.kr>
2022-04-19 11:19:45 +0900

    fix: change to absolute path

--- src/components/examples/scExample.tsx
+++ src/components/examples/scExample.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { Check } from '../../assets/icons';
+import { Check } from 'assets/icons';
 
 const Heading = styled.h1`
   text-decoration: underline;
