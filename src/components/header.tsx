caa5f0d2fe4075e5f4ddab2d21d1464cbf1dfc10
SongYi Kim <sykim@mz.co.kr>
2022-04-19 12:11:08 +0900

    feat: add header

---
+++ src/components/header.tsx
@@ -0,0 +1,81 @@
+import tw from 'twin.macro';
+import styled from 'styled-components';
+import { Logo, IcExternal } from 'assets/icons';
+import LinkButton from 'components/linkButton';
+
+const StyledHeader = styled.header`
+  display: flex;
+  align-items: center;
+  width: 1120px;
+  height: 64px;
+  margin: 0 auto;
+  nav {
+    display: flex;
+    align-items: center;
+    flex-grow: 1;
+    height: 100%;
+    ul {
+      display: flex;
+      height: 100%;
+      margin-left: auto;
+    }
+  }
+`;
+
+const StyledNavItem = styled.a`
+  ${tw`text-gray-500`}
+  display: flex;
+  align-items: center;
+  height: 100%;
+  padding-right: 16px;
+  padding-left: 16px;
+  
+  &:hover {
+    ${tw`text-violet-400`}
+  }
+`;
+
+const StyledIcExternal = styled(IcExternal)`
+  margin-left: 4px;
+  path {
+    fill: #484848;
+    
+    ${StyledNavItem}:hover & {
+      fill: #6638B6;
+    }
+  }
+`;
+
+const StyledLinkButtonWrapper = styled.div`
+  margin: 0 16px;
+`;
+
+// const StyledLinkButton = styled(LinkButton)`
+// `
+
+const Header = () => (
+    <StyledHeader>
+        <h1 className="border border-gray-600"><Logo /></h1>
+        <nav>
+            <ul>
+                <li tw="text-violet-500"><StyledNavItem href="/">Products</StyledNavItem></li>
+                <li><StyledNavItem href="/">Pricing</StyledNavItem></li>
+                <li><StyledNavItem href="/">FAQ</StyledNavItem></li>
+                <li><StyledNavItem href="/">Contact</StyledNavItem></li>
+            </ul>
+            <StyledLinkButtonWrapper>
+                <LinkButton color="violet-400" size="medium">Talk to Sales</LinkButton>
+            </StyledLinkButtonWrapper>
+            <StyledNavItem href="/">
+                Docs
+                <StyledIcExternal />
+            </StyledNavItem>
+            <StyledNavItem href="/">
+                Community
+                <StyledIcExternal />
+            </StyledNavItem>
+        </nav>
+    </StyledHeader>
+);
+
+export default Header;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from 'next/app';
 import { ReactElement } from 'react';
 import { GlobalStyles } from 'twin.macro';
 import Head from 'next/head';
+import Header from 'components/header';
 
 function MyApp({ Component, pageProps }: AppProps): ReactElement {
     return (
@@ -11,6 +12,7 @@ function MyApp({ Component, pageProps }: AppProps): ReactElement {
                 <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard-dynamic-subset.css" />
                 <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@300;400;500;600;700&display=swap" />
             </Head>
+            <Header />
             <Component {...pageProps} />
             <GlobalStyles />
         </>
