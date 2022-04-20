d591ee9fa4e8e17f65489cce26cb22f83dfeb394
SongYi Kim <sykim@mz.co.kr>
2022-04-20 15:01:18 +0900

    fix: fix type in linkButton, linkText

--- src/components/linkText.tsx
+++ src/components/linkText.tsx
@@ -1,36 +1,36 @@
 import tw from 'twin.macro';
 import styled from 'styled-components';
 import { IcArrowRight } from 'assets/icons';
+import { ReactNode } from 'react';
 
 interface LinkTextProps {
     href: string;
-    children: string;
+    children: ReactNode;
 }
 
 const StyledLinkText = styled.a`
-  ${tw`inline-flex`}
-`;
-
-const StyledText = styled.span`
-  ${tw`inline-flex text-violet-400 text-lg hover:(text-violet-400 underline)`}
-`;
-
-const StyledSVGWrapper = styled.span`
-    ${tw`inline-flex items-center`}
-    > svg {
-      ${tw`inline-block text-violet-400`}
+  ${tw`inline-flex items-center`}
+  > svg {
+    transition: margin-left .25s;
+    path {
+      ${tw`text-violet-400 fill-current`}
     }
-  
-  ${StyledLinkText}:hover & {
-    margin-left: 4px;
-    transition: margin-left .3s;
+  }
+  .text {
+    ${tw`inline-flex text-violet-400 text-lg hover:(text-violet-400 underline)`}
+  }
+  &:hover {
+    > svg {
+       margin-left: 4px;
+      transition: margin-left .25s;
+    }
   }
 `;
 
 const LinkText = ({ href = '/', children = 'link' }: LinkTextProps) => (
     <StyledLinkText href={href}>
-        <StyledText>{children}</StyledText>
-        <StyledSVGWrapper><IcArrowRight color="#6638B6" /></StyledSVGWrapper>
+        <span className="text">{children}</span>
+        <IcArrowRight />
     </StyledLinkText>
 );
 
