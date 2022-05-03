4501a8aae6cbe8063f7079dbd9e6a80a4ea48b5b
SongYi Kim <sykim@mz.co.kr>
2022-05-03 17:56:14 +0900

    feat: add FAQ page

--- src/pages/faq/index.tsx
+++ src/pages/faq/index.tsx
@@ -1,4 +1,5 @@
 import TopicSection from 'components/TopicSection';
+import FAQSection from 'components/FAQ';
 
 const Pricing = () => {
     const TopicData = {
@@ -6,8 +7,38 @@ const Pricing = () => {
         description: 'Check frequently asked questions and \nGet your questions answered.',
     };
 
+    const FAQData = [
+        {
+            title: 'What payment methods do you offer?',
+            content: 'This is content',
+        },
+        {
+            title: 'How do I switch to a different plan?',
+            content: 'This is content',
+        },
+        {
+            title: 'Can I buy credits instead?',
+            content: 'This is content',
+        },
+        {
+            title: 'Do you have pricing for high-volume email senders?',
+            content: 'This is content',
+        },
+        {
+            title: 'How can I buy SpaceONE apps for advanced features?',
+            content: 'This is content',
+        },
+        {
+            title: 'What payment methods do you offer?',
+            content: 'This is content',
+        },
+    ];
+
     return (
-        <TopicSection title={TopicData.title} description={TopicData.description} />
+        <>
+            <TopicSection title={TopicData.title} description={TopicData.description} />
+            <FAQSection FAQData={FAQData} />
+        </>
     );
 };
 
